b822108d6cd18d25634cc43f11c2538418a7d821
dericward <dericward@hotmail.com>
2023-03-04 19:50:35 +0000

    basic Employee code with name, email, id

--- index.js
+++ index.js
@@ -11,5 +11,32 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./src/page-template.js");
 
 
+const questions = [
+    {
+        name: 'name',
+        message: 'Enter employee name:',
+        type: 'input'
+    },
+    {
+        name: 'email',
+        message: 'Enter email:',
+        type: 'email'
+    },
+    {
+        name: 'id',
+        message: 'Enter ID:',
+        type: 'input'
+    },
+];
+
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
+
+
+// check if user has finished building team, if so, quit and
+// generate HTML
+
+// pass all employees array to the render function
+
+// rendered file name is team.html
+// check the providd 'outputPath' to target team.html
 
